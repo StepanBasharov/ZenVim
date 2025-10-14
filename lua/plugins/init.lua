12bccf9991e769f24089f45da8b80f200fb893b3
Stepan Basharov <stepan.basharov@rt.ru>
2025-10-14 19:32:06 +0300

    sql support

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,5 @@
 return {
-    {
+        {
     "neovim/nvim-lspconfig",
     config = function()
         local lspconfig = require("lspconfig")
@@ -59,89 +59,94 @@ return {
         })
     end,
 },
-     {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline",
-            "saadparwaiz1/cmp_luasnip",
-            "L3MON4D3/LuaSnip",
-            "onsails/lspkind-nvim", -- иконки
-        },
-        config = function()
-            local cmp = require("cmp")
-            local luasnip = require("luasnip")
-            local lspkind = require("lspkind")
-
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<Tab>"] = cmp.mapping.select_next_item(),
-                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                }),
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                }, {
-                    { name = "buffer" },
-                }),
-                formatting = {
-                    fields = { "kind", "abbr", "menu" }, -- порядок колонок
-                    format = function(entry, vim_item)
-                        -- Добавляем иконки и цвет через lspkind
-                        vim_item.kind = lspkind.presets.default[vim_item.kind] .. " " .. vim_item.kind
-
-                        -- Пишем источник справа
-                        vim_item.menu = ({
-                            buffer = "[Buf]",
-                            nvim_lsp = "[LSP]",
-                            luasnip = "[Snip]",
-                            path = "[Path]",
-                            cmdline = "[Cmd]",
-                        })[entry.source.name]
-
-                        return vim_item
-                    end,
-                },
-                experimental = {
-                    ghost_text = true,
-                },
-                window = {
-                    completion = cmp.config.window.bordered(),    -- рамка вокруг completion
-                    documentation = cmp.config.window.bordered(), -- рамка вокруг документации
-                },
-            })
-
-            cmp.setup.cmdline("/", {
-                mapping = cmp.mapping.preset.cmdline(),
-            sources = {
-            { name = "buffer" }
-             }
-            })
-
-            -- Командная строка
-            cmp.setup.cmdline(":", {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = {
-                    { name = "path" },
-                    { name = "cmdline" },
-                },
-            })
-        end,
-    },
     {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip",
+    "onsails/lspkind-nvim",
+    "kristijanhusak/vim-dadbod-completion", -- 👈 добавь это
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    local lspkind = require("lspkind")
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+      }, {
+        { name = "buffer" },
+      }),
+      formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+          vim_item.kind = lspkind.presets.default[vim_item.kind] .. " " .. vim_item.kind
+          vim_item.menu = ({
+            buffer = "[Buf]",
+            nvim_lsp = "[LSP]",
+            luasnip = "[Snip]",
+            path = "[Path]",
+            cmdline = "[Cmd]",
+            ["vim-dadbod-completion"] = "[DB]", -- 👈 красиво подпишем источник
+          })[entry.source.name]
+          return vim_item
+        end,
+      },
+      experimental = {
+        ghost_text = true,
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+    })
+
+    -- 🔍 Поиск
+    cmp.setup.cmdline("/", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = { { name = "buffer" } },
+    })
+
+    -- 💻 Командная строка
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "path" },
+        { name = "cmdline" },
+      },
+    })
+
+    -- 🧠 Настройка для SQL файлов
+    cmp.setup.filetype({ "sql", "mysql", "plsql" }, {
+      sources = cmp.config.sources({
+        { name = "vim-dadbod-completion" },
+        { name = "buffer" },
+      }),
+    })
+  end,
+},
+     {
     "nvimtools/none-ls.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
